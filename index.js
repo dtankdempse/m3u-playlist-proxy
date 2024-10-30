33b3963132e3cac95e7e5b50fb29b2af83dcb165
Reuben <dtankdempsey@gmail.com>
2024-10-30 13:21:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ http.createServer(async (req, res) => {
       const html = `<!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta name="robots" content="noindex, nofollow">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>M3U Playlist Proxy</title>
